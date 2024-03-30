47fa1b29276dcce60502b8f5354d4193e9bffeca
Gayathri Karunarathna <karunarathna.gayathri15@gmail.com>
2024-03-30 09:29:41 +0530

    customized commit message

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9*zneftkzh6)&lvhyb2yme=c0&6rjf^g-nyud2v7jk*sb$x#hd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['web-production-12223.up.railway.app']
 
@@ -81,9 +81,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
